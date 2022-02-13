173a81c4d0e6252f0fce751c33ff02fa6ad10b4f
Jeffery Cosby <jeffery.cosby@omes.ok.gov>
2022-02-12 23:43:57 -0600

    add task

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -5,8 +5,15 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        private ApplicationDbContext _context;
+
         [BindProperty]
         public Task NewTask { get; set; }
+
+        public CreateTaskModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         public void OnGet()
         {
@@ -14,7 +21,10 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
+            _context.Tasks.Add(new Task());
+            _context.SaveChanges();
             return RedirectToPage("Index");
+
         }
     }
 }
